ce5bd2c94a74d93fea11f43d1bea49786f88f4d6
Ava Zahedi <AvaZahedi2023@u.northwestern.edu>
2023-10-03 13:45:25 -0500

    fixed offset issue. removed offset in grasp_frame_transform. included (commented) way of checking links w collision geometry

--- deep_grasp_task/src/deep_pick_place_task.cpp
+++ deep_grasp_task/src/deep_pick_place_task.cpp
@@ -229,6 +229,14 @@ void DeepPickPlaceTask::init()
       stage->allowCollisions(
           object, t.getRobotModel()->getJointModelGroup(hand_group_name_)->getLinkModelNamesWithCollisionGeometry(),
           true);
+
+      // std::cout << "############################################\n";
+      // auto info = t.getRobotModel()->getJointModelGroup(hand_group_name_)->getLinkModelNamesWithCollisionGeometry();
+      // for (auto val : info) {
+      //   std::cout << val << "\n";
+      // }
+      // std::cout << "############################################\n";
+
       grasp->insert(std::move(stage));
     }
 
